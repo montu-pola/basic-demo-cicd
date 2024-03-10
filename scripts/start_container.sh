b647232a7c36a777555e3ec91518fde8fd89add1
montu-pola <162019398+montu-pola@users.noreply.github.com>
2024-03-10 23:34:12 +0000

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull montudipola99/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d - 5000:5000 montudipola99/simple-python-flask-app
+docker run -d -p 5000:5000 montudipola99/simple-python-flask-app
